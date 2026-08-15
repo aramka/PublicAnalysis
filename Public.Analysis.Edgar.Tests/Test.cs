f818a4b5413cfecb02a65bb96132331bc525f528
andrewr <andrew.ramka@gmail.com>
2026-08-15 17:41:29 -0400

    Add test project for Edgar

---
+++ Public.Analysis.Edgar.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Public.Analysis.Edgar.Tests
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
