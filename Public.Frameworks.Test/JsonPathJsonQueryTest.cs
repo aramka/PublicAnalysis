13a0a79b3eabf6e3fcb7ba5cd0bc61ca2f353f78
andrewr <andrew.ramka@gmail.com>
2026-08-18 18:04:12 -0400

    Test JsonPathJsonQuery

--- Public.Frameworks.Test/JsonPathJsonQueryTest.cs
+++ Public.Frameworks.Test/JsonPathJsonQueryTest.cs
@@ -23,5 +23,44 @@ namespace Public.Frameworks.Tests
             Assert.AreEqual(1, result.Count());
             Assert.AreEqual(100, result.First().GetValue<int>());
         }
+
+        [TestMethod]
+        public void Query_JsonObjectPath()
+        {
+            // Arrange
+            var jsonQuery = new JsonPathJsonQuery();
+            var jsonNode = JsonNode.Parse("{\"AAPL\":{\"facts\":{\"us-gaap\":{\"AccountsPayable\":{\"units\":{\"USD\":100,\"EUR\":90}}}}}}");
+            var path = new string[] { "AAPL", "facts", "us-gaap", "AccountsPayable", "units" };
+            // Act
+            var result = jsonQuery.Query(jsonNode!, path);
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+            var unitsNode = result.First() as JsonObject;
+            Assert.IsNotNull(unitsNode);
+            Assert.AreEqual(100, unitsNode["USD"]!.GetValue<int>());
+            Assert.AreEqual(90, unitsNode["EUR"]!.GetValue<int>());
+        }
+        [TestMethod]
+        public void Query_ArrayPath()
+        {
+            // Arrange
+            var jsonQuery = new JsonPathJsonQuery();
+            var jsonNode = JsonNode.Parse("{\"AAPL\":{\"facts\":{\"us-gaap\":{\"AccountsPayable\":[{\"units\":{\"USD\":100}},{\"units\":{\"USD\":200}}]}}}}");
+            var path = new string[] { "AAPL", "facts", "us-gaap", "AccountsPayable" };
+            // Act
+            var result = jsonQuery.Query(jsonNode!, path);
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.HasCount(1, result);
+            var accountsPayableArray = result.First() as JsonArray;
+            Assert.AreEqual(2, accountsPayableArray?.Count());
+            var firstItem = accountsPayableArray![0] as JsonObject;
+            var secondItem = accountsPayableArray[1] as JsonObject;
+            Assert.IsNotNull(firstItem);
+            Assert.IsNotNull(secondItem);
+            Assert.AreEqual(100, firstItem["units"]!["USD"]!.GetValue<int>());
+            Assert.AreEqual(200, secondItem["units"]!["USD"]!.GetValue<int>());
+        }
     }
 }
