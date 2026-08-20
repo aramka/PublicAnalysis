d623d286252d5f4aaeb017fb3b3a85a72962146c
andrewr <andrew.ramka@gmail.com>
2026-08-20 10:43:48 -0400

    Test validation of chaining of query expressions

--- Public.Frameworks.Test/ThrowIfNotValidConsecutiveJsonQueryExpressionsTest.cs
+++ Public.Frameworks.Test/ThrowIfNotValidConsecutiveJsonQueryExpressionsTest.cs
@@ -8,17 +8,17 @@ namespace Public.Frameworks.Tests
     [TestClass]
     public class ThrowIfNotValidConsecutiveJsonQueryExpressionsTest
     {
+        Dictionary<(Type?, Type?), bool> cases = new Dictionary<(Type?, Type?), bool>();
         public ThrowIfNotValidConsecutiveJsonQueryExpressionsTest()
         {
-            Dictionary<(Type?, Type?), bool> cases = new Dictionary<(Type?, Type?), bool>();
 
             cases.Add((null, null), false);
             cases.Add((null, typeof(JsonQueryFilter)), true);
             cases.Add((null, typeof(JsonQueryLogicalAnd)), false);
-            cases.Add((null,typeof(JsonQueryLogicalOr)), false);
+            cases.Add((null, typeof(JsonQueryLogicalOr)), false);
             cases.Add((null, typeof(JsonQueryPath)), true);
             cases.Add((typeof(JsonQueryFilter), null), false);
-            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryFilter)), true);
+            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryFilter)), false);
             cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryLogicalAnd)), true);
             cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryLogicalOr)), true);
             cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryPath)), true);
@@ -37,6 +37,39 @@ namespace Public.Frameworks.Tests
             cases.Add((typeof(JsonQueryPath), typeof(JsonQueryLogicalAnd)), false);
             cases.Add((typeof(JsonQueryPath), typeof(JsonQueryLogicalOr)), false);
             cases.Add((typeof(JsonQueryPath), typeof(JsonQueryPath)), true);
+        }
+
+        [TestMethod]
+        public void TestThrowIfNotValid()
+        {
+            var getInstanceOfJsonQueryExpression = new Func<Type?,string, IJsonQueryExpression?>((Type? type, string caseName) =>
+            {
+                return type switch
+                {
+                    null => null,
+                    Type t when t == typeof(JsonQueryFilter) => new JsonQueryFilter(caseName, JsonQueryFilterOperators.Eq, caseName),
+                    Type t when t == typeof(JsonQueryLogicalAnd) => new JsonQueryLogicalAnd(),
+                    Type t when t == typeof(JsonQueryLogicalOr) => new JsonQueryLogicalOr(),
+                    Type t when t == typeof(JsonQueryPath) => new JsonQueryPath(),
+                    _ => throw new NotImplementedException($"Type {type} is not implemented.")
+                };
+            });
+
+            var cases = this.cases
+                .Select((kvp) => new { First = getInstanceOfJsonQueryExpression(kvp.Key.Item1, $"Case_{kvp.Key.Item1}_{kvp.Key.Item2}"), Second = getInstanceOfJsonQueryExpression(kvp.Key.Item2, $"Case_{kvp.Key.Item2}_{kvp.Key.Item1}"), Expected = kvp.Value });
+        
+            var underTest = new ThrowIfNotValidConsecutiveJsonQueryExpressions();
+            foreach (var testCase in cases)
+            {
+                if (testCase.Expected)
+                {
+                    Assert.Throws<ArgumentException>(() => underTest.ThrowIfNotValid(testCase.First, testCase.Second));
+                }
+                else
+                {
+                    underTest.ThrowIfNotValid(testCase.First, testCase.Second);
+                }
+            }
         }
     }
 }
--- Public.Frameworks/JsonQuery/ThrowIfNotValidConsecutiveJsonQueryExpressions.cs
+++ Public.Frameworks/JsonQuery/ThrowIfNotValidConsecutiveJsonQueryExpressions.cs
@@ -6,8 +6,43 @@ namespace Public.Frameworks.JsonQuery
 {
     public class ThrowIfNotValidConsecutiveJsonQueryExpressions : IThrowIfNotValidConsecutiveJsonQueryExpressions
     {
+        Dictionary<(Type?, Type?), bool> cases = new Dictionary<(Type?, Type?), bool>();
+        public ThrowIfNotValidConsecutiveJsonQueryExpressions() {
+
+            cases.Add((null, null), false);
+            cases.Add((null, typeof(JsonQueryFilter)), true);
+            cases.Add((null, typeof(JsonQueryLogicalAnd)), false);
+            cases.Add((null, typeof(JsonQueryLogicalOr)), false);
+            cases.Add((null, typeof(JsonQueryPath)), true);
+            cases.Add((typeof(JsonQueryFilter), null), false);
+            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryFilter)), false);
+            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryLogicalAnd)), true);
+            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryLogicalOr)), true);
+            cases.Add((typeof(JsonQueryFilter), typeof(JsonQueryPath)), true);
+            cases.Add((typeof(JsonQueryLogicalAnd), null), false);
+            cases.Add((typeof(JsonQueryLogicalAnd), typeof(JsonQueryFilter)), true);
+            cases.Add((typeof(JsonQueryLogicalAnd), typeof(JsonQueryLogicalAnd)), false);
+            cases.Add((typeof(JsonQueryLogicalAnd), typeof(JsonQueryLogicalOr)), false);
+            cases.Add((typeof(JsonQueryLogicalAnd), typeof(JsonQueryPath)), false);
+            cases.Add((typeof(JsonQueryLogicalOr), null), false);
+            cases.Add((typeof(JsonQueryLogicalOr), typeof(JsonQueryFilter)), true);
+            cases.Add((typeof(JsonQueryLogicalOr), typeof(JsonQueryLogicalAnd)), false);
+            cases.Add((typeof(JsonQueryLogicalOr), typeof(JsonQueryLogicalOr)), false);
+            cases.Add((typeof(JsonQueryLogicalOr), typeof(JsonQueryPath)), false);
+            cases.Add((typeof(JsonQueryPath), null), false);
+            cases.Add((typeof(JsonQueryPath), typeof(JsonQueryFilter)), true);
+            cases.Add((typeof(JsonQueryPath), typeof(JsonQueryLogicalAnd)), false);
+            cases.Add((typeof(JsonQueryPath), typeof(JsonQueryLogicalOr)), false);
+            cases.Add((typeof(JsonQueryPath), typeof(JsonQueryPath)), true);
+
+        }
         public void ThrowIfNotValid(IJsonQueryExpression? current, IJsonQueryExpression? next)
         {
+            var key = (current?.GetType(), next?.GetType());
+            if (cases.TryGetValue(key, out bool expected) && expected)
+            {
+                throw new ArgumentException($"Consecutive JSON query expressions of types '{current?.GetType().Name}' and '{next?.GetType().Name}' are not valid.");
+            }
         }
     }
 }
