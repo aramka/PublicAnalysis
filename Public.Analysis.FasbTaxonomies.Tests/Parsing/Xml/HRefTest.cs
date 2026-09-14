70b7fbf915767bb2a98b0b557d5b6b3a6bfa5229
andrewr <andrew.ramka@gmail.com>
2026-09-14 13:17:51 -0400

    Href xml attribute

---
+++ Public.Analysis.FasbTaxonomies.Tests/Parsing/Xml/HRefTest.cs
@@ -0,0 +1,38 @@
+﻿using Public.Analysis.FasbTaxonomies.Parsing.Xml;
+using Public.Analysis.FasbTaxonomies.Tests.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Public.Analysis.FasbTaxonomies.Parsing.Xrbl
+{
+    [TestClass]
+    public class HRefTest
+    {
+
+        [TestMethod]
+        [DataRow($"theLocation#theAnchor", "theLocation", "theAnchor")]
+        public void LocationAndAnchor(string hRefValue, string expectedLocation, string expectedAnchor)
+        {
+            HRef hRef = new HRef(hRefValue);
+
+            Assert.AreEqual(expectedLocation, hRef.Location());
+            Assert.AreEqual(expectedAnchor, hRef.Anchor());
+        }
+        [TestMethod]
+        [DataRow("theLocation#",DisplayName = "location#")]
+        [DataRow("#theAnchor", DisplayName = "#theAnchor")]
+        [DataRow("noHashSymbol", DisplayName = "noHashSymbol")]
+        [DataRow("", DisplayName = "empty string")]
+        [DataRow("    ", DisplayName = "whitespace")]
+        [DataRow("  #  ", DisplayName = "whitespace hash whitespace")]
+        [DataRow("#  ", DisplayName = "hash whitespace")]
+        [DataRow("  # ", DisplayName = "whitespace hash")]
+        [DataRow("#", DisplayName = "hash")]
+        public void HRefValueNotValid_Throws(string invalidHRef)
+        {
+            Assert.ThrowsExactly<InvalidOperationException>(() => new HRef(invalidHRef));
+        }
+    }
+}
---
+++ Public.Analysis.FasbTaxonomies/Parsing/Xml/HRef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Public.Analysis.FasbTaxonomies.Parsing.Xml
+{
+    public class HRef
+    {
+        private string hRef;
+        private string[] parts;
+
+        public HRef(string hRef)
+        {
+            this.hRef = hRef;
+            this.parts = hRef?.Split('#', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+
+            if(this.parts.Length != 2)
+            {
+                throw new InvalidOperationException("Invalid hRef. hRef must be of the form {location}#{anchor}");
+            }
+        }
+
+        public string Anchor() => parts[1];
+
+        public string? Location() => parts[0];
+    }
+}
