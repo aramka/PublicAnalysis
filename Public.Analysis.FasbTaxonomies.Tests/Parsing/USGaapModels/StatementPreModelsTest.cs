f2ce5b528fd612660b0242c44a327b5c15e0aa34
andrewr <andrew.ramka@gmail.com>
2026-09-15 12:59:20 -0400

    Statement models for use with xml deserializer

---
+++ Public.Analysis.FasbTaxonomies.Tests/Parsing/USGaapModels/StatementPreModelsTest.cs
@@ -0,0 +1,44 @@
+﻿using AwesomeAssertions;
+using Public.Analysis.FasbTaxonomies.Parsing.USGaapModels;
+using System.Xml.Serialization;
+
+namespace Public.Analysis.FasbTaxonomies.Parsing.Statements
+{
+    [TestClass]
+    public class StatementPreModelsTest
+    {
+        [Ignore]
+        [TestMethod]
+        public void AllFasbStatementsParse()
+        {
+
+            XmlSerializer serializer = new XmlSerializer(typeof(linkbase));
+            string usGaap2026StmDirectory = @"fasb us-gaap-2026\stm directory";
+            var statementFiles = Directory.GetFiles(usGaap2026StmDirectory, "*pre-2026.xml").ToList();
+            int i = 0;
+            HashSet<string> hasPresentationLink = new HashSet<string>();
+            foreach (string filePath in statementFiles)
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    linkbase result = (linkbase)serializer.Deserialize(reader)!;
+                    
+                    if(result.presentationLink is not null)
+                    {
+                        var hRefs = result.presentationLink.loc.ToHRef().Where(l => string.IsNullOrWhiteSpace(l.Location)  || string.IsNullOrWhiteSpace(l.Anchor));
+
+                        hRefs.Should().BeEmpty();
+
+                        hasPresentationLink.Add(filePath);
+                    }
+                }
+                ++i;
+            }
+
+            var noPresentationLink = statementFiles.Where(f => !hasPresentationLink.Contains(f)).ToList();
+
+            noPresentationLink.Should().BeEmpty();
+
+        }
+    }
+}
---
+++ Public.Analysis.FasbTaxonomies/Parsing/USGaapModels/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Public.Analysis.FasbTaxonomies.Parsing.USGaapModels;
+using Public.Analysis.FasbTaxonomies.Parsing.Xml;
+
+namespace Public.Analysis.FasbTaxonomies.Parsing.USGaapModels
+{
+    public static class Extensions
+    {
+        public static IEnumerable<HRef> ToHRef(this IEnumerable<linkbasePresentationLinkLoc>? loc) => loc?.Select(l => new HRef(l.href)) ?? Enumerable.Empty<HRef>();
+    }
+}
