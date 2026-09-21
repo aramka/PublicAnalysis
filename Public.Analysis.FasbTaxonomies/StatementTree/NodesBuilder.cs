0ac6481522ed5519040e5cc56aa7f5da50135b80
andrewr <andrew.ramka@gmail.com>
2026-09-21 17:33:34 -0400

    NodesBuilder, add more cases. Handle case of only parent nodes

--- Public.Analysis.FasbTaxonomies/StatementTree/NodesBuilder.cs
+++ Public.Analysis.FasbTaxonomies/StatementTree/NodesBuilder.cs
@@ -10,58 +10,67 @@ namespace Public.Analysis.FasbTaxonomies.StatementTree
     {
         public Dictionary<ElementId, IStatementNode> BuildNodes(PresentationLink presentationLink, IReadOnlyDictionary<ElementId, IXsElement> elementsByElementId, IReadOnlyDictionary<ElementId, string> labelsByElementId)
         {
+            Dictionary<string, IEnumerable<Arc>> arcsByFrom = presentationLink.Arcs.GroupBy(a => a.From).ToDictionary(a => a.Key, a => a.Select(b => b));
+
             Dictionary<string, Loc> locsByXLinkLabel = presentationLink.Locs.ToDictionary(l => l.XLinkLabel);
 
             Dictionary<ElementId, IStatementNode> nodes = new Dictionary<ElementId, IStatementNode>();
 
-            foreach(var arc in presentationLink.Arcs)
+            foreach (Loc loc in presentationLink.Locs) //foreach(var arc in presentationLink.Arcs)
             {
-                if(!locsByXLinkLabel.TryGetValue(arc.From, out Loc? parentLoc))
+                //if(!locsByXLinkLabel.TryGetValue(arc.From, out Loc? parentLoc))
+                //{
+                //    throw new InvalidOperationException($"Locator From->{arc.From} not found for arc relation From->{arc.From}, To->{arc.To}");
+                //}
+
+
+                if (!elementsByElementId.TryGetValue(loc.ElementId, out IXsElement? element))
                 {
-                    throw new InvalidOperationException($"Locator From->{arc.From} not found for arc relation From->{arc.From}, To->{arc.To}");
-                }
-                if(!locsByXLinkLabel.TryGetValue(arc.To, out Loc? childLoc))
-                {
-                    throw new InvalidOperationException($"Locator To->{arc.To} not found for arc relation From->{arc.From}, To->{arc.To}");
-                }
-                if(!elementsByElementId.TryGetValue(parentLoc.ElementId, out IXsElement? parentElement))
-                {
-                    throw new InvalidOperationException($"Element {parentLoc.ElementId} not found.");
-                }
-                if(!elementsByElementId.TryGetValue(childLoc.ElementId, out IXsElement? childElement))
-                {
-                    throw new InvalidOperationException($"Element {childLoc.ElementId} not found.");
-                }
-                if (!labelsByElementId.TryGetValue(parentLoc.ElementId, out string? parentLabel))
-                {
-                    throw new InvalidOperationException($"Label for {parentLoc.ElementId} not found.");
-                }
-                if (!labelsByElementId.TryGetValue(childLoc.ElementId, out string? childLabel))
-                {
-                    throw new InvalidOperationException($"Label {childLoc.ElementId} not found.");
+                    throw new InvalidOperationException($"Element {loc.ElementId} not found.");
                 }
 
-                BuildAndAddParentAndChildNodes(parentElement, parentLabel, childElement, childLabel, arc.Order, nodes);
+                if (!labelsByElementId.TryGetValue(loc.ElementId, out string? label))
+                {
+                    throw new InvalidOperationException($"Label for {loc.ElementId} not found.");
+                }
+
+                if (!nodes.TryGetValue(loc.ElementId, out IStatementNode? node))
+                {
+                    node = new StatementNode(element, label, 0);
+                    nodes.Add(node.ElementId, node);
+                }
+
+                if (arcsByFrom.TryGetValue(loc.XLinkLabel, out IEnumerable<Arc>? parentChildArcs))
+                {
+                    foreach (Arc arc in parentChildArcs)
+                    {
+                        if (!locsByXLinkLabel.TryGetValue(arc.To, out Loc? childLoc))
+                        {
+                            throw new InvalidOperationException($"Loc for {nameof(Arc)}.To->{arc.To} not found for arc relation {nameof(Arc)}.From->{arc.From}, {nameof(Arc)}.To->{arc.To}");
+                        }
+                        if (!elementsByElementId.TryGetValue(childLoc.ElementId, out IXsElement? childElement))
+                        {
+                            throw new InvalidOperationException($"Element {childLoc.ElementId} not found.");
+                        }
+                        if (!labelsByElementId.TryGetValue(childLoc.ElementId, out string? childLabel))
+                        {
+                            throw new InvalidOperationException($"Label {childLoc.ElementId} not found.");
+                        }
+
+                        if (!nodes.TryGetValue(childElement.ElementId, out IStatementNode? childNode))
+                        {
+                            childNode = new StatementNode(childElement, childLabel, 0);
+                            nodes.Add(childNode.ElementId, childNode);
+                        }
+                        node.AddChild(childNode);
+                        childNode.ParentElementId = node.ElementId;
+                        childNode.Order = arc.Order;
+                    }
+                }
+
             }
 
             return nodes;
-        }
-
-        private void BuildAndAddParentAndChildNodes(IXsElement parentElement, string parentLabel, IXsElement childElement, string childLabel, decimal order, Dictionary<ElementId, IStatementNode> existingNodes)
-        {
-            if(!existingNodes.TryGetValue(parentElement.ElementId, out IStatementNode? parentNode))
-            {
-                parentNode = new StatementNode(parentElement, parentLabel, 0);
-                existingNodes.Add(parentNode.ElementId, parentNode);
-            }
-            if(!existingNodes.TryGetValue(childElement.ElementId, out IStatementNode? childNode))
-            {
-                childNode = new StatementNode(childElement, childLabel, 0);
-                existingNodes.Add(childNode.ElementId, childNode);
-            }
-            parentNode.AddChild(childNode);
-            childNode.ParentElementId = parentNode.ElementId;
-            childNode.Order = order;
         }
     }
 }
